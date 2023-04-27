826b868682d1c9ed285be0038a330e44d5ecd2cf
Crafted By Little Dragons <elsandkls@kidshideaway.net>
2023-04-27 01:19:04 -0400

    initial commit
    updated name references to use getCurrentResourceName() for isolation, and updated config file to generic version.

--- config.lua
+++ config.lua
@@ -1,14 +1,19 @@
 
 Config = {}
-Config.ScriptName = GetCurrentResourceName() 
+Config.ScriptName = GetCurrentResourceName()
+    -- TODO
+    -- CAMERA FACE NPC
+    -- NPC ANIMATION
 
--- TODO
--- CAMERA FACE NPC
--- NPC ANIMATION
-
---menu position
--- "center" / "top-left" / "top-right"
+    --menu position
+    -- "center" / "top-left" / "top-right"
 Config.Align = "top-left"
+
+Config.defaultlang = "en_lang"
+
+       -- open stores
+Config.Key = 0x760A9C6F --[G]
+
 
 --Webhook Section, description is in translation
 Config.UseWebhook = false -- Use webhook
@@ -24,66 +29,38 @@ Config.WebhookLogo = ""
 Config.WebhookLogo2 = ""
 Config.WebhookAvatar = ""
 
-Config.defaultlang = "en_lang"
-
--- open stores
-Config.Key = 0x760A9C6F --[G]
-
 
     --- STORES ---
 
-Config.Stores = { 
+Config.Stores = {
+ 
 -----------------------------------------------------------------------------
 --------------------------------------Valentine------------------------------
 -----------------------------------------------------------------------------
-    ValBlacksmith = {
+ 
+    ValPharmacy = {
         blipAllowed = true,
-        BlipName = "Blacksmith Shop",
-        storeName = "Valentine Blacksmith Shop",
-        PromptName = "Blacksmith Shop",
-        sprite = -758970771,
-        x = -360.44, y = 794.71, z = 116.24, h = 336.49,
-        distanceOpenStore = 3.0,
+        BlipName = "Pharmacy",
+        storeName = "Valentine Pharmacy",
+        PromptName = "Pharmacy",
+        sprite = 1109348405,
+        x = -288.03, y = 804.23, z = 119.39, h = 297.74,
+        distanceOpenStore = 2.5,
         NpcAllowed = true,
-        NpcModel = "S_M_M_LiveryWorker_01",
+        NpcModel = "U_M_M_RHDDOCTOR_01",
         AllowedJobs = {}, -- jobs allowed
         JobGrade = 0,
-        category = { "Tools" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
-        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
-        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        category = { "Medicine","Firstaid"},
+        storeType = { "Buy", "Sell" }, -- only one type
+        StoreHoursAllowed = false,
         RandomPrices = false,
         StoreOpen = 7, -- am
         StoreClose = 21 -- pm
 
-    }, 
+    },  
 }
 
------------------------------------------------------------------------------
--------------------------------------ITEMS-----------------------------------
------------------------------------------------------------------------------
 
-    -- ItemLable = translate here
-    -- itemName = same as in your databse
-    -- curencytype = "cash" or "gold" only use one.
-    -- price = numbers only
-    -- desc = a description of the item
-    -- category = where the item will be displayed at 
-
-BlackSmith_ShopItems_SELL = {         
-       -- Tools
-    { itemLabel = "Pickaxe", itemName = "pickaxe", currencyType = "cash", sellprice = 5, randomprice = math.random(30, 55), desc = "Sell a Pickaxe", category = "Tools" },
-    { itemLabel = "Hatchet", itemName = "hatchet", currencyType = "cash", sellprice = 5, randomprice = math.random(30, 55), desc = "Sell a Garden Hoe", category = "Tools" }       
-} 
-
------------------------------------------------------------------------------
---------------------------------------SELL ITEMS ----------------------------
------------------------------------------------------------------------------
-Config.SellItems = {       
-    -----------------------------------------------------------------------------
-    --------------------------------------Valentine------------------------------
-    -----------------------------------------------------------------------------        
-        ValBlacksmith = BlackSmith_ShopItems_SELL,  
-}
 -----------------------------------------------------------------------------
 -------------------------------------ITEMS-----------------------------------
 -----------------------------------------------------------------------------
@@ -94,36 +71,121 @@ Config.SellItems = {
     -- price = numbers only
     -- desc = a description of the item
     -- category = where the item will be displayed at
-    BlackSmith_ShopItems_BUY = {
-            -- Tools
-           { itemLabel = "Pickaxe", itemName = "pickaxe", currencyType = "cash", buyprice = 20, randomprice = math.random(30, 55), desc = "Buy a Pickaxe", category = "Tools" },
-            { itemLabel = "Hatchet", itemName = "hatchet", currencyType = "cash", buyprice = 20, randomprice = math.random(30, 55), desc = "Buy a Garden Hoe", category = "Tools" }            
-     }
+ 
+    Pharmacy_Items_SELL = {            
+    --Medicine
+{ itemLabel = "Acetaminophen", itemName = "tylenol", currencyType = "cash", sellprice = 15, randomprice = math.random(40, 65), desc = "Sell Acetaminophen", category = "Medicine" },
+{ itemLabel = "Plastic Surgery Appointement", itemName = "secondchance", currencyType = "cash", sellprice = 150, randomprice = math.random(40, 65), desc = "Sell Plastic Surgery Appointment", category = "Medicine" },
+--Firstaid
+{ itemLabel = "Bandage", itemName = "bandage", currencyType = "cash", sellprice = 10, randomprice = math.random(40, 65), desc = "Sell Bandage", category = "Firstaid" },
+{ itemLabel = "Anti-Poison", itemName = "antipoison", currencyType = "cash", sellprice = 10, randomprice = math.random(40, 65), desc = "Sell Anti-Poison", category = "Firstaid" }
+
+ }
+ 
+-----------------------------------------------------------------------------
+--------------------------------------SELL ITEMS ----------------------------
+-----------------------------------------------------------------------------
+Config.SellItems = {
+      
+    -----------------------------------------------------------------------------
+    --------------------------------------Armadillo------------------------------
+    -----------------------------------------------------------------------------
+ 
+    -----------------------------------------------------------------------------
+    --------------------------------------Blackwater------------------------------
+    -----------------------------------------------------------------------------
+  
+    -----------------------------------------------------------------------------
+    --------------------------------------Rhodes---------------------------------
+    -----------------------------------------------------------------------------
+ 
+    -----------------------------------------------------------------------------
+    --------------------------------------St-Denis-------------------------------
+    -----------------------------------------------------------------------------
+ 
+        StDenisPharmacy = Pharmacy_Items_SELL,
+    -----------------------------------------------------------------------------
+    --------------------------------------Strawberry-----------------------------
+    -----------------------------------------------------------------------------
+ 
+        StrawbPharmacy = Pharmacy_Items_SELL,
+    -----------------------------------------------------------------------------
+    --------------------------------------Tumbleweed-----------------------------
+    -----------------------------------------------------------------------------
+  
+    -----------------------------------------------------------------------------
+    --------------------------------------Valentine------------------------------
+    -----------------------------------------------------------------------------
+ 
+        ValPharmacy = Pharmacy_Items_SELL,
+    -----------------------------------------------------------------------------
+    --------------------------------------Vanhorn--------------------------------
+    -----------------------------------------------------------------------------
+         
+}
+
+-------------------------------------ITEMS-----------------------------------
+-----------------------------------------------------------------------------
+
+    -- ItemLable = translate here
+    -- itemName = same as in your databse
+    -- curencytype = "cash" or "gold" only use one.
+    -- price = numbers only
+    -- desc = a description of the item
+    -- category = where the item will be displayed at
+  
+   
+    Pharmacy_Items_BUY = {            
+    --Medicine
+        { itemLabel = "Acetaminophen", itemName = "tylenol", currencyType = "cash", buyprice = 15, randomprice = math.random(40, 65), desc = "Buy Acetaminophen", category = "Medicine" },
+        { itemLabel = "Plastic Surgery Appointement", itemName = "secondchance", currencyType = "cash", buyprice = 150, randomprice = math.random(40, 65), desc = "Buy Plastic Surgery Appointement", category = "Medicine" },
+    --Firstaid
+        { itemLabel = "Bandage", itemName = "bandage", currencyType = "cash", buyprice = 10, randomprice = math.random(40, 65), desc = "Buy Bandage", category = "Firstaid" },
+        { itemLabel = "Anti-Poison", itemName = "antipoison", currencyType = "cash", buyprice = 10, randomprice = math.random(40, 65), desc = "Buy Anti-Poison", category = "Firstaid" }
+    }
+
+        
 -----------------------------------------------------------------------------
 --------------------------------------BUY ITEMS ----------------------------
 -----------------------------------------------------------------------------
-Config.BuyItems = {     
+Config.BuyItems = {
+    
+    -----------------------------------------------------------------------------
+    --------------------------------------Armadillo- ----------------------------
+    -----------------------------------------------------------------------------
+ 
+    -----------------------------------------------------------------------------
+    --------------------------------------Blackwater------------------------------
+    -----------------------------------------------------------------------------
+ 
+    -----------------------------------------------------------------------------
+    --------------------------------------Rhodes---------------------------------
+    -----------------------------------------------------------------------------
+ 
+    -----------------------------------------------------------------------------
+    --------------------------------------St-Denis-------------------------------
+    -----------------------------------------------------------------------------
+ 
+        StDenisPharmacy = Pharmacy_Items_BUY,
+    -----------------------------------------------------------------------------
+    --------------------------------------Strawberry-----------------------------
+    -----------------------------------------------------------------------------
+ 
+        StrawbPharmacy = Pharmacy_Items_BUY,
+    -----------------------------------------------------------------------------
+    --------------------------------------Tumbleweed-----------------------------
+    -----------------------------------------------------------------------------
+ 
     -----------------------------------------------------------------------------
     --------------------------------------Valentine------------------------------
-    -----------------------------------------------------------------------------       
-        ValBlacksmith = BlackSmith_ShopItems_BUY, 
+    -----------------------------------------------------------------------------
+ 
+        ValPharmacy = Pharmacy_Items_BUY,
+    -----------------------------------------------------------------------------
+    --------------------------------------Vanhorn--------------------------------
+    -----------------------------------------------------------------------------
+ 
+    
+ 
+    
 }
-
---- Want the pre configured data for the stores? 
---- Little Creek -- Admin an dConcept Owner of Little Creek, Tillie 
-------- has put together an amazing set of stores and items.
-
---- Purchase her configuration files for vorp_stores at our website.
---- https://craftedbylittledragons.net/vorp-store-configuration-files-by-tillie-little-creek/
-
---- The config file for this specific store.
---- https://craftedbylittledragons.net/product/vorp_store_blacksmith-config-lua/
-
---- The related crafting files.
---- https://craftedbylittledragons.net/product/vorp_crafting_blacksmith-config-lua/
-
---- All crafting files. 
---- https://craftedbylittledragons.net/vorp-crafting-configuration-files-by-tillie-little-creek/
-
---- Bundles are available here:
---- https://craftedbylittledragons.net/vorp-configuration-files-bundles-by-tillie-little-creek/
